380953688c9acee40ae325f1d4a475dcee68de0a
Albert Ispiryan <59164972+AlbertIspiryan-dev@users.noreply.github.com>
2023-07-12 14:13:57 +0400

    Classic widget support
    Added support for the classic widget and messaging types

--- src/index.js
+++ src/index.js
@@ -15,10 +15,11 @@ module.exports = {
     window.zESettings = options.settings;
 
     const root = new Vue();
-
+    const type = options.type;
     let isLoaded = false;
 
     root.isLoaded = () => isLoaded;
+
     root.load = zendeskKey => {
       if (isLoaded) {
         return;
@@ -29,8 +30,7 @@ module.exports = {
       script.async = true;
       script.id = "ze-snippet";
       const actualZendeskKey = zendeskKey || options.key;
-      script.src =
-        "https://static.zdassets.com/ekr/snippet.js?key=" + actualZendeskKey;
+      script.src = "https://static.zdassets.com/ekr/snippet.js?key=" + actualZendeskKey;
 
       if (options.nonce) {
         script.setAttribute('nonce', options.nonce)
@@ -44,41 +44,46 @@ module.exports = {
         isLoaded = true;
 
         if (options.hideOnLoad) {
-          root.hide();
+          window.zE(type, 'hide');
         }
 
         root.$emit("loaded", event);
 
-        window.zE("messenger:on", "open", () => {
-          root.$emit("open")
+        window.zE(type + ":on", "open", () => {
+          root.$emit("open");
         })
 
-        window.zE("messenger:on", "close", () => {
-          root.$emit("close")
+        window.zE(type + ":on", "close", () => {
+          if (type === 'webWidget') {
+            window.zE(type, 'hide');
+          }
+
+          root.$emit("close");
         })
 
-        window.zE("messenger:on", "unreadMessages", (count) => {
-          root.$emit("unreadMessages", count)
-        })
+        root.open = () => {
+          if (type === 'webWidget') {
+            window.zE(type, 'show')
+          }
+
+          window.zE(type, 'open');
+        }
+        root.close = () => window.zE(type, 'close');
+        root.hide = () => window.zE(type, 'hide');
+        root.show = () => window.zE(type, 'show');
+        
+        Object.defineProperty(root, "zE", {
+          get: function get() {
+            return window.zE;
+          }
+        });
+  
+        Vue.prototype.$zendesk = root;
       };
     };
 
     if (!options.disabled) {
       root.load(options.key);
     }
-
-    root.open = () => window.zE('messenger', 'open');
-    root.close = () => window.zE('messenger', 'close');
-    root.hide = () => window.zE('messenger', 'hide');
-    root.show = () => window.zE('messenger', 'show');
-    
-
-    Object.defineProperty(root, "zE", {
-      get: function get() {
-        return window.zE;
-      }
-    });
-
-    Vue.prototype.$zendesk = root;
   }
 };
